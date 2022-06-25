7284a6c5f8e84abb1585c84a367613e57ae20998
wixarika <agahydzik2@gmail.com>
2022-06-25 10:58:58 +0200

    jquery nav-links commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ function buttonClicked() {
 function buttonToChangeTextClicked() {
     document.getElementsById('first-column').innerHTML = "JavaScript text"
 }
+
+$(function(){
+    var pages = ['index','about','form']
+    var pathname = window.location.pathname;
+
+    $('.nav-link').each(function(i){
+        if(pathname.includes(pages[i])){
+            $(this).addClass('active');
+            $(this).attr('aria-current','page');
+        }else if(this.className.includes('active')){
+            $(this).removeClass('active');
+        }
+
+    });
+});
